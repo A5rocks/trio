a6b44676a0d2181afc42d9d506e17742d5ca7a8b
A5rocks <git@helvetica.moe>
2026-04-21 13:00:04 -0400

    Remove coverage.process_startup() call

--- ci.sh
+++ ci.sh
@@ -16,14 +16,7 @@ python -m uv pip install "trio @ $wheel_package" -c test-requirements.txt
 python -m uv pip install coverage -c test-requirements.txt
 echo "::endgroup::"
 
-echo "::group::Setup for tests"
 INSTALLDIR=$(python -c "import os, trio; print(os.path.dirname(trio.__file__))")
-cp pyproject.toml "$INSTALLDIR"  # TODO: remove this
-
-# support subprocess spawning with coverage.py
-echo "import coverage; coverage.process_startup()" | tee -a "$INSTALLDIR/../sitecustomize.py"
-
-echo "::endgroup::"
 
 for i in {1..100}; do
     COVERAGE_PROCESS_START=$(pwd)/pyproject.toml \
